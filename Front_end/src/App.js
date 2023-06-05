28d528c98478784ce6a2ab088d122243cdbeaa25
HoVanDi <hovandideveloper@gmail.com>
2023-06-05 09:30:49 +0700

    done

---
+++ Front_end/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import ShowProduct from './components/ShowProduct';
+
+
+
+function App() {
+  return (
+    <div>
+      <ShowProduct></ShowProduct>
+    </div>
+  );
+}
+
+export default App;
